42211a0134f75a37b681b6f9987d5b252d946f9b
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-05-22 19:42:21 +0530

    Redux toolkit and cart

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,20 @@ import App from './App';
 import {ThemeProvider} from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import {theme} from './theme';
+import {Provider} from 'react-redux';
+import { ConfigureStore } from '@reduxjs/toolkit';
+import cartReducer from './state';
+const store = ConfigureStore({
+  reducer: {cart:cartReducer}
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
     </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
