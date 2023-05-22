80c550338d533ebbd05a43efd2d33d88caee9abc
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-05-22 18:23:15 +0530

    theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
-
+import { useEffect } from "react";
+import{BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
+import Home from "./scenes/home/Home";
+const ScrollToTop = () => {
+  const {pathname} = useLocation();
+  useEffect(() => {
+    window.scrollTo(0,0);
+  }, [pathname]);
+  return null;
+}
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="about" element={<h1>About</h1>} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
