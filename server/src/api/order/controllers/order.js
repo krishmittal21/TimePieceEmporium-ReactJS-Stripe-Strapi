4a8733c59ffad69dbd3eaed4717c76b17be87aa1
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-05-24 23:13:22 +0530

    stripe and checkout

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -1,4 +1,5 @@
 'use strict';
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 /**
  * order controller
@@ -6,4 +7,40 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
+  async create(ctx) {
+    const {products,userName,email}=ctx.request.body;
+    try{
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi.services.product.findOne({ id: product.id });
+          return {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: item.name,
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: product.count,
+          }
+        }
+      ));
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        customer_email: email,
+        mode:"payment",
+        success_url: "http://localhost:3000/checkout/success",
+        cancel_url: "http://localhost:3000",
+        line_items: lineItems,
+      });
+      await strapi.services.service("api::order.order").create({
+        data :{userName, products, stripeSessionId: session.id},
+      });
+      return {id:session.id};
+    }catch(error){
+      ctx.response.status = 500;
+      return {error:{message:"Something went wrong"}};
+    }
+  }
+}));
