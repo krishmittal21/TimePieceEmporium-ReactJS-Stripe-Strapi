bc499b58ab45a2c62826c1aacacfe6c1a3999a46
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-05-24 21:17:41 +0530

    Update Checkout.jsx

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -1,3 +1,12 @@
+import { useSelector } from "react-redux";
+import { Box, Button, Stepper, Step, StepLabel } from "@mui/material";
+import { Formik } from "formik";
+import { useState } from "react";
+import * as yup from "yup";
+import { shades } from "../../theme";
+import Payment from "./Payment";
+import Shipping from "./Shipping";
+import { loadStripe } from "@stripe/stripe-js";
 const Checkout = () => {
     return (
         <div>
