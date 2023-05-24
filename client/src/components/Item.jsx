b78323b181cc320a963d5b3a597c3852f90af94a
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-05-24 20:25:45 +0530

    update ShoppingList.jsx

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,95 +1,85 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import {IconButton, Box,Typography, useTheme, Button} from "@mui/material";
-import AddIcon from '@mui/material/Add';
-import RemoveIcon from '@mui/material/Remove';
-import {shades} from "../theme";
-import {addToCart} from "../state";
-import {useNavigate} from "react-router-dom";
-const Item =({item,width})=>{
+    import { useState } from "react";
+    import { useDispatch } from "react-redux";
+    import { IconButton, Box, Typography, useTheme, Button } from "@mui/material";
+    import AddIcon from "@mui/icons-material/Add";
+    import RemoveIcon from "@mui/icons-material/Remove";
+    import { shades } from "../theme";
+    import { addToCart } from "../state";
+    import { useNavigate } from "react-router-dom";
+
+    const Item = ({ item, width }) => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const [count,setCount] = useState(1);
-    const [isHovered,setIsHovered] = useState(false);
-    const{
-        palette:{neutral}
+    const [count, setCount] = useState(1);
+    const [isHovered, setIsHovered] = useState(false);
+    const {
+        palette: { neutral },
     } = useTheme();
-    const {category,name,image,price} = item.attributes;
-    const{
-        data:{
-            attributes:{
-                formats:{
-                    medium:{url},
-                }
-            }
-        }
-    }= image;
-    return(
+
+    const { category, price, name, image } = item.attributes;
+    const url = image?.data?.attributes?.formats?.medium?.url || '';
+
+    return (
         <Box width={width}>
+        <Box
+            position="relative"
+            onMouseOver={() => setIsHovered(true)}
+            onMouseOut={() => setIsHovered(false)}
+        >
+            <img
+            alt={item.name}
+            width="300px"
+            height="400px"
+            src={`http://localhost:1337${url}`}
+            onClick={() => navigate(`/item/${item.id}`)}
+            style={{ cursor: "pointer" }}
+            />
             <Box
-            position="relative" 
-            onMouseOver={()=>setIsHovered(true)} 
-            onMouseOut={()=>setIsHovered(false)}
+            display={isHovered ? "block" : "none"}
+            position="absolute"
+            bottom="10%"
+            left="0"
+            width="100%"
+            padding="0 5%"
             >
-                <img 
-                alt ={item.name}
-                width="300px"
-                height="400px"
-                src={`http://localhost:1337${url}`}
-                onClick={()=>navigate(`/item/${item.id}`)}
-                style={{cursor:"pointer"}}
-                />
+            <Box display="flex" justifyContent="space-between">
                 <Box
-                    display={isHovered?"blocked":"none"}
-                    position="absolute"
-                    left={0}
-                    width="100%"
-                    padding="0 5%"
-                    bottom="10%"
+                display="flex"
+                alignItems="center"
+                backgroundColor={shades.neutral[100]}
+                borderRadius="3px"
                 >
-                    <Box display="flex" justifyContent="space-between">
-                        <Box
-                            display="flex"
-                            alignItems="center"
-                            backgroundColor={shades.neutral[100]}
-                            borderRadius="3px"
-                        >
-                                <IconButton
-                            onClick={() =>
-                                setCount(Math.max( count - 1,1))
-                            }
-                            >
-                            <RemoveIcon />
-                            </IconButton>
-                            <Typography color={shades.primary[300]}>{count}</Typography>
-                            <IconButton
-                            onClick={() =>
-                                setCount(Math.max( count + 1))
-                            }
-                            >
-                            <AddIcon />
-                            </IconButton>
-                        </Box>
-                        {/* button*/ }
-                        <Button
-                            onClick={()=>{dispatch(addToCart({item:{...item,count}}));}}
-                            sx={{backgroundColor:shades.primary[300],color:"white"}}
-                        >
-                        </Button>
-                    </Box>
+                <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
+                    <RemoveIcon />
+                </IconButton>
+                <Typography color={shades.primary[300]}>{count}</Typography>
+                <IconButton onClick={() => setCount(count + 1)}>
+                    <AddIcon />
+                </IconButton>
                 </Box>
+                <Button
+                onClick={() => {
+                    dispatch(addToCart({ item: { ...item, count } }));
+                }}
+                sx={{ backgroundColor: shades.primary[300], color: "white" }}
+                >
+                Add to Cart
+                </Button>
             </Box>
-            <Box
-                mt="3px"
-            >
-                <Typography variant="subtitile2" color={neutral.dark}>
-                    {category.replace(/([A-Z])/g,"$1")
-                    .replace(/^./,str=>str.toUpperCase())}
-                </Typography>
-                <Typography>{name}</Typography>
-                <Typography fontWeight="bold">${price}</Typography>
             </Box>
         </Box>
-    )
-};
-export default Item;
+
+        <Box mt="3px">
+            <Typography variant="subtitle2" color={neutral.dark}>
+            {category
+                .replace(/([A-Z])/g, " $1")
+                .replace(/^./, (str) => str.toUpperCase())}
+            </Typography>
+            <Typography>{name}</Typography>
+            <Typography fontWeight="bold">${price}</Typography>
+        </Box>
+        </Box>
+    );
+    };
+
+    export default Item;
