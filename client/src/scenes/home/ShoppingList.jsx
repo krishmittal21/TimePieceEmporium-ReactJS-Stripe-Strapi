b9ce4e841706f1045043a0911530b88c8dcafb4f
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-05-24 19:55:02 +0530

    Update ShoppingList.jsx

---
+++ client/src/scenes/home/ShoppingList.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import Item from "../../components/Item";
+import { Typography } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useDispatch, useSelector } from "react-redux";
+import { setItems } from "../../state";
+const ShoppingList = () => {
+
+};
+export default ShoppingList;
